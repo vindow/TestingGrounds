8d85e7b7e74bde569d17aeb650b0fde566bba7dc
Alvin Yang <alvin.yang73@gmail.com>
2017-12-28 16:16:47 -0800

    Added proper procedural generation of props in the level. They can be configured to spawn at a randomized scale and also spawn at random rotations in yaw.

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -12,24 +12,10 @@ ATile::ATile()
 
 }
 
-void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
-{
-	int32 NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
-	for (int32 i = 0; i < NumberToSpawn; i++)
-	{
-		auto SpawnPoint = FMath::RandPointInBox(FBox(FVector(0.0f, -2000.0f, 0.0f), FVector(4000.0f, 2000.0f, 0.0f)));
-		AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ToSpawn);
-		SpawnedActor->SetActorRelativeLocation(SpawnPoint);
-		SpawnedActor->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
-	}
-}
-
 // Called when the game starts or when spawned
 void ATile::BeginPlay()
 {
 	Super::BeginPlay();
-	CastSphere(GetActorLocation(), 300.0f);
-	CastSphere(GetActorLocation() + FVector(0, 0, 500), 300.0f);
 }
 
 // Called every frame
@@ -39,17 +25,49 @@ void ATile::Tick(float DeltaTime)
 
 }
 
-bool ATile::CastSphere(FVector Location, float Radius)
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn, float MinScale, float MaxScale, float Radius)
+{
+	int32 NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+	float RandomScale = FMath::RandRange(MinScale, MaxScale);
+	for (int32 i = 0; i < NumberToSpawn; i++)
+	{
+		FVector SpawnPoint;
+		if (FindEmptyLocation(SpawnPoint, Radius * RandomScale))
+		{
+			float RandomRotation = FMath::RandRange(-180.0f, 180.0f);
+			PlaceActor(ToSpawn, SpawnPoint, RandomRotation , RandomScale);
+		}
+	}
+}
+
+bool ATile::FindEmptyLocation(FVector& OutLocation, float Radius)
+{
+	const uint32 MAX_ATTEMPTS = 10;
+	for (uint32 i = 0; i < MAX_ATTEMPTS; i++)
+	{
+		FVector RandomPoint = FMath::RandPointInBox(FBox(FVector(0.0f, -2000.0f, 0.0f), FVector(4000.0f, 2000.0f, 0.0f)));
+		if (CanSpawnAtLocation(RandomPoint, Radius))
+		{
+			OutLocation = RandomPoint;
+			return true;
+		}
+	}
+	return false;
+}
+
+void ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint, float Rotation, float Scale)
+{
+	AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ToSpawn);
+	SpawnedActor->SetActorRelativeLocation(SpawnPoint);
+	SpawnedActor->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
+	SpawnedActor->SetActorRotation(FRotator(0, Rotation, 0));
+	SpawnedActor->SetActorRelativeScale3D(FVector(Scale));
+}
+
+bool ATile::CanSpawnAtLocation(FVector Location, float Radius)
 {
 	FHitResult HitResult;
-	bool HasHit = GetWorld()->SweepSingleByChannel(HitResult, Location, Location, FQuat::Identity, ECollisionChannel::ECC_Camera, FCollisionShape::MakeSphere(Radius));
-	if (HasHit)
-	{
-		DrawDebugSphere(GetWorld(), Location, Radius, 32, FColor(255, 0, 0), true, -1.0f, (uint8)'\000', 1.0f);
-	}
-	else
-	{
-		DrawDebugSphere(GetWorld(), Location, Radius, 32, FColor(0, 255, 0), true, -1.0f, (uint8)'\000', 1.0f);
-	}
-	return HasHit;
+	FVector GlobalLocation = ActorToWorld().TransformPosition(Location);
+	bool HasHit = GetWorld()->SweepSingleByChannel(HitResult, GlobalLocation, GlobalLocation, FQuat::Identity, ECollisionChannel::ECC_GameTraceChannel2, FCollisionShape::MakeSphere(Radius));
+	return !HasHit;
 }
--- Source/TestingGrounds/Terrain/Tile.h
+++ Source/TestingGrounds/Terrain/Tile.h
@@ -16,7 +16,7 @@ public:
 	ATile();
 
 	UFUNCTION(BlueprintCallable, Category = "Setup")
-	void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn);
+	void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn = 1, int MaxSpawn = 1, float MinScale = 1.0f, float MaxScale = 1.0f, float Radius = 500.0f);
 
 protected:
 	// Called when the game starts or when spawned
@@ -27,6 +27,10 @@ public:
 	virtual void Tick(float DeltaTime) override;
 private:
 
-	bool CastSphere(FVector Location, float Radius);
+	bool CanSpawnAtLocation(FVector Location, float Radius);
 	
+	bool FindEmptyLocation(FVector& OutLocation, float Radius);
+
+	void PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint, float Rotation, float Scale);
+
 };
